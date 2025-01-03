7811991d36bc4019e7bcc71b1a83f2559e13e2e1
filtered <176114999+webfiltered@users.noreply.github.com>
2025-01-03 17:26:43 +1100

    [Desktop] Add maintenance / validation view
    Add maint screen to router
    Add desktop screen to router

---
+++ src/views/DesktopStartView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="font-sans w-screen h-screen mx-0 grid items-center justify-center text-neutral-300 bg-neutral-900 dark-theme pointer-events-auto overflow-y-auto"
+  >
+    <div
+      class="max-w-screen-sm w-screen p-8 relative col-start-1 row-start-1 mx-auto"
+    >
+      <!-- TODO: Impl. progress updates once added to desktop. -->
+      <ProgressBar mode="indeterminate" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ProgressBar from 'primevue/progressbar'
+</script>
